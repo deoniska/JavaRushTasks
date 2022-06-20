28a55ce2cff3eed39a249c42a39423d190380aec
Denis Rjashenko <deoniska@me.com>
2022-06-20 22:44:21 +0300

    monday

--- 1.JavaSyntax/src/com/javarush/task/pro/task03/task0311/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0311/Solution.java
@@ -9,10 +9,16 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
-        System.out.println("числа равны");
-        //напишите тут ваш код
-        System.out.println("числа не равны");
+        Scanner scanner = new Scanner(System.in);
+        double first = scanner.nextDouble();
+        double second = scanner.nextDouble();
 
-        //new testing
+        if (Math.abs(first - second) < 0.000001) {
+            System.out.println("числа равны");
+            //напишите тут ваш код
+        } else {
+            System.out.println("числа не равны");
+        }
+
     }
 }
