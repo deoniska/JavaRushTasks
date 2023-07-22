4b032ca5fc02e4d6885d7dab2efbe31a6e314ebf
DenisR <deoniska@me.com>
2023-07-22 06:55:22 +0300

    Commit for test2

--- 2.JavaCore/src/com/javarush/task/task13/task1304/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1304/Solution.java
@@ -19,7 +19,7 @@ public class Solution {
 
         @Override
         public void onSelect() {
-            System.out.println("test2");
+            System.out.println("test3");
         }
 
         @Override
